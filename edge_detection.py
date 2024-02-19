34d3e15cd7fc13a59ae53689ca280bc2d196177f
Pooja Tambe <64680838+poojatambe@users.noreply.github.com>
2024-02-19 19:04:37 +0530

    Added edge detection code with streamlit UI.

---
+++ edge_detection.py
@@ -0,0 +1,111 @@
+import cv2
+import numpy as np
+
+
+class EdgeDetect:
+    def __init__(self, image):
+        self.img = image
+
+    def sobel_edge(self, thresh, kernel_size, opencv):
+        # img = cv2.imread(self.path)
+        gray_im = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        if opencv == False:
+            if kernel_size == 3:
+                gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+                gy = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+                gradient_x = cv2.filter2D(gray_im, cv2.CV_64F, gx)
+                gradient_y = cv2.filter2D(gray_im, cv2.CV_64F, gy)
+                magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+                threshold = thresh
+                edge = magnitude > threshold
+                sobel_edge = edge.astype(np.uint8) * 255
+                return sobel_edge, gradient_x, gradient_y
+            else: 
+                gx = np.array([[-1, -2, 0, 2, 1], [-2, -3, 0, 3, 2], 
+                               [-3, -5, 0, 5, 3], [-2, -3, 0, 3, 2], 
+                               [-1, -2, 0, 2, 1]])
+                gy = np.array([[1, 2, 3, 2, 1], [2, 3, 5, 3, 2], 
+                               [0, 0, 0, 0, 0], [-2, -3, -5, -3, -2], 
+                               [-1, -2, -3, -2, -1]])
+                gradient_x = cv2.filter2D(gray_im, cv2.CV_64F, gx)
+                gradient_y = cv2.filter2D(gray_im, cv2.CV_64F, gy)
+                magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+                threshold = thresh
+                edge = magnitude > threshold
+                sobel_edge = edge.astype(np.uint8) * 255
+                return sobel_edge, gradient_x, gradient_y
+        else: 
+            # cv2 sobel implmentation
+            sobelx = cv2.Sobel(src=gray_im, ddepth=cv2.CV_64F,
+                               dx=1, dy=0, ksize=kernel_size)
+            sobely = cv2.Sobel(src=gray_im, ddepth=cv2.CV_64F,
+                               dx=0, dy=1, ksize=kernel_size)
+            sobelxy = cv2.Sobel(src=gray_im, ddepth=cv2.CV_64F,
+                                dx=1, dy=1, ksize=kernel_size)
+            sobelx = cv2.convertScaleAbs(sobelx)
+            sobely = cv2.convertScaleAbs(sobely)
+            return sobelxy, sobelx, sobely
+
+    def robert_edge(self, thresh):
+        """
+        Robert edge detection.
+        """
+        # img = cv2.imread(self.path)
+        gray_im = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        
+        gx = np.array([[0, 1], [-1, 0]])
+        gy = np.array([[1, 0], [0, -1]])
+
+        gradient_x = cv2.filter2D(gray_im, cv2.CV_64F, gx)
+        gradient_y = cv2.filter2D(gray_im, cv2.CV_64F, gy)
+        magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+        threshold = thresh
+        edge = magnitude > threshold
+        robert_edge = edge.astype(np.uint8) * 255
+        return robert_edge, gradient_x, gradient_y     
+
+    def prewitt_edge(self, thresh):
+        """
+        Prewitt edge detection
+        """
+        # img = cv2.imread(self.path)
+        gray_im = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+        gx = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+        gy = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+
+        gradient_x = cv2.filter2D(gray_im, cv2.CV_64F, gx)
+        gradient_y = cv2.filter2D(gray_im, cv2.CV_64F, gy)
+        magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+        threshold = thresh
+        edge = magnitude > threshold
+        prewitt_edge = edge.astype(np.uint8) * 255
+        return prewitt_edge, gradient_x, gradient_y 
+
+    def laplacian_edge(self, kernel_size):
+        """
+        Laplacian edge detector
+        """
+        # img = cv2.imread(self.path)
+        gray_im = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+        # remove noise
+        n_img = cv2.GaussianBlur(gray_im, (3, 3), 0)
+        log_img = cv2.Laplacian(n_img, ddepth=cv2.CV_16S, ksize=kernel_size)
+        log_img = cv2.convertScaleAbs(log_img)
+        return log_img
+
+    def canny_edge(self, t1, t2, aperture_size, l2norm):
+        """
+        Canny edge detection
+        """
+        # img = cv2.imread(self.path)
+        gray_im = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+        # remove noise
+        n_img = cv2.GaussianBlur(gray_im, (3, 3), 0)
+
+        canny_img = cv2.Canny(n_img, t1, t2, 
+                              apertureSize=aperture_size, L2gradient=l2norm)
+        return canny_img
+        
---
+++ main.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from edge_detection import EdgeDetect
+from PIL import Image
+import numpy as np
+
+
+st.set_page_config("Edge Detection in CV", 
+                   layout='wide', 
+                   initial_sidebar_state='expanded')
+
+st.title('Edge Detection in CV')
+st.markdown('Explore Gradient and Gaussian edge detection methods.')
+
+
+file = st.file_uploader('Upload image', type=['jpg', 'png'])
+with st.sidebar:
+    st.subheader('Edge Detection Methods:')
+    option = st.radio('Select method: ',
+             ['None', 'Sobel', 'Prewitt', 'Robert',
+               'Laplacian of Gaussian', 'Canny detector'])
+
+# First convert image to array format. Based on method adjust parameters
+# and get output of edge detection for different methods.
+if file is not None:
+    image = Image.open(file)
+    image = np.asarray(image)
+    method = EdgeDetect(image)
+    if option == 'None':
+        st.subheader('Select Edge Detection Method')
+    elif option == 'Sobel':
+        st.subheader('Sobel Edge Detection')
+        t_val = st.slider('Select threshold: ', 0, 255, 20, 1)
+        c1, c2 = st.columns(2)
+        kernel = int(c1.radio('Select Kernel size:', ['3', '5']))
+        opencv = c2.radio('Select Opencv:', [True, False])
+        out1, out2, out3 = method.sobel_edge(thresh=t_val, 
+                                             kernel_size=kernel, 
+                                             opencv=opencv)
+        col1, col2, col3 = st.columns(3)
+        col1.image(out1, 'Sobel output', clamp=True)
+        col2.image(out2, 'X gradient', clamp=True)
+        col3.image(out3, 'Y gradient', clamp=True)
+    elif option == 'Prewitt':
+        st.subheader('Prewitt Edge Detection')
+        t_val = st.slider('Select threshold: ', 0, 255, 20, 1)
+        out1, out2, out3 = method.prewitt_edge(thresh=t_val)
+        col1, col2, col3 = st.columns(3)
+        col1.image(out1, 'Prewitt output', clamp=True)
+        col2.image(out2, 'X gradient', clamp=True)
+        col3.image(out3, 'Y gradient', clamp=True)
+    elif option == 'Robert':
+        st.subheader('Robert Edge Detection')
+        t_val = st.slider('Select threshold: ', 0, 255, 20, 1)
+        out1, out2, out3 = method.robert_edge(thresh=t_val)
+        col1, col2, col3 = st.columns(3)
+        col1.image(out1, 'Robert output', clamp=True)
+        col2.image(out2, 'X gradient', clamp=True)
+        col3.image(out3, 'Y gradient', clamp=True)
+    elif option == 'Laplacian of Gaussian':
+        st.subheader('Laplacian of Gaussian')
+        kernel = int(st.radio('Select Kernel:', ['3', '5']))
+        out = method.laplacian_edge(kernel)
+        st.image(out, 'Laplacian of Gaussian', clamp=True)
+    else:
+        st.subheader('Canny Edge Detection')
+        min_v = st.slider('Set minimum threshold:', 0, 255, 10, 1)
+        max_v = st.slider('Set maximum threshold:', 0, 255, 50, 1)
+        l2norm = st.radio('Select L2 gradient:', [True, False])
+        c1, c2, c3 = st.columns(3)
+        out1 = method.canny_edge(min_v, max_v, 3, l2norm)
+        c1.image(out1, 'aperture size 3', clamp=True)
+        out2 = method.canny_edge(min_v, max_v, 5, l2norm)
+        c2.image(out2, 'aperture size 5', clamp=True)
+        out3 = method.canny_edge(min_v, max_v, 7, l2norm)
+        c3.image(out3, 'aperture size 7', clamp=True)
